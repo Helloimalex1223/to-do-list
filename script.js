73e0d93c37150d444779e07adb3dd11ec1336c6e
Helloimalex1223 <plattaalex@gmail.com>
2025-10-27 08:52:39 -0400

    Update UI/reorganize code

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+// ===== Top Input and Control Elements =====
 const toDoName = document.getElementById("toDoListName");
 const addItemButton = document.getElementById("addItem");
-const toDoContainer = document.getElementById("addCardHere");
+const deleteCompletedButton = document.getElementById("deleteCompleted");
+const displayAllCards = document.getElementById("displayAllCards");
+
 const lowPriorityButton = document.getElementById("lowPriorityFilter");
 const mediumPriorityButton = document.getElementById("mediumPriorityFilter");
 const highPriorityButton = document.getElementById("highPriorityFilter");
-const deleteCompletedButton = document.getElementById("deleteCompleted");
-const displayAllCards = document.getElementById("displayAllCards");
+
+const toDoContainer = document.getElementById("addCardHere");
 let selectedParentElement;
 
-//defines the card number. The value starts from the last key in the localStorage. If there are no keys in the localStorage, start from 0.
-//Set the id to the cardNumber variable. If the id already exists in the localStorage, increment the id
+
+// ===== Local Storage Setup =====
 let cardNumber;
 let localStorageLength = localStorage.length;
-
-// localStorage.clear();
 
 console.log("local storage length " + localStorageLength);
 
@@ -32,6 +33,7 @@ else if(localStorageLength > 0) {
 }
 
 
+// ===== Add Card Button Logic =====
 addItemButton.addEventListener("click", () =>
 {
     if(toDoName.value != "")
@@ -42,13 +44,11 @@ addItemButton.addEventListener("click", () =>
     }
 });
 
+
+// ===== Add Card Function =====
 function addCard(cardName, cardPriority, checkBoxValue, datePicked, fromStorage = false)
 {
-    //create a constant of this card's particular card number. This is so when you go to retrieve a value in localStorage, you don't use the already incremented cardNumber value
     const currentCardNumber = cardNumber;
-
-
-    //need to set the default values if the card information is not defined by the user (like in the case of the usre manually adding a card vs. reading from localStorage)
     let myCardPriority = cardPriority || undefined;
     let myCheckBoxValue = checkBoxValue || false;
     let myDatePicked = datePicked || undefined;
@@ -56,66 +56,97 @@ function addCard(cardName, cardPriority, checkBoxValue, datePicked, fromStorage 
     let newCard = document.createElement("div");
     newCard.classList.add("card");
     newCard.setAttribute("id", cardNumber);
-
-    //sets the card's priority (when read from localStorage)
     if (myCardPriority && myCardPriority !== "select") {
         newCard.classList.add(myCardPriority);
     }
 
+    // ===== Left Side of Card (Title + Priority + Due Date) =====
+    let myCardNameDiv = document.createElement("div");
+    myCardNameDiv.classList.add("cardNameDiv");
+    
     let myCardNameValue = document.createElement("h2");
     myCardNameValue.classList.add("innerCardName");
     myCardNameValue.innerHTML = cardName;
+    myCardNameDiv.appendChild(myCardNameValue);
 
-    newCard.appendChild(myCardNameValue);
-    toDoContainer.appendChild(newCard);
-
-    //add a selection menu div
     let selectionDiv = document.createElement("div");
     selectionDiv.classList.add("selectionContainer");
 
-    //add the priority selection menu in the DOM
+    let leftSideCardDiv = document.createElement("div");
+    leftSideCardDiv.classList.add("leftSideCardDiv");
+    leftSideCardDiv.appendChild(myCardNameDiv);
+    leftSideCardDiv.appendChild(selectionDiv);
+
+    newCard.appendChild(leftSideCardDiv);
+    toDoContainer.appendChild(newCard);
+
+    // ===== Priority Menu =====
     let priorityMenuLabel = document.createElement("label");
     priorityMenuLabel.setAttribute("for", "priorityOptions");
     priorityMenuLabel.innerHTML = "Priority";
     selectionDiv.appendChild(priorityMenuLabel);
-    
     
     let priorityMenuList = document.createElement("select");
     priorityMenuList.setAttribute("name", "priorityOptions");
     priorityMenuList.setAttribute("id", "priorityOptions");
     selectionDiv.appendChild(priorityMenuList);
 
-    //menu options for the selection menu
     let selectPriority = document.createElement("option");
     selectPriority.setAttribute("value", "select");
     selectPriority.innerHTML = "Select";
-
     priorityMenuList.appendChild(selectPriority);
     
     let lowPriority = document.createElement("option");
     lowPriority.setAttribute("value", "low");
     lowPriority.innerHTML = "Low";
-
     priorityMenuList.appendChild(lowPriority);
 
     let mediumPriority = document.createElement("option");
     mediumPriority.setAttribute("value", "medium");
     mediumPriority.innerHTML = "Medium";
-
     priorityMenuList.appendChild(mediumPriority);
 
     let highPriority = document.createElement("option");
     highPriority.setAttribute("value", "high");
     highPriority.innerHTML = "High";
-
     priorityMenuList.appendChild(highPriority);
 
+    // ===== Due Date =====
+    let myDateDiv = document.createElement("div");
+    myDateDiv.classList.add("dateDivContainer");
 
-    //checkbox div
-    let checkboxDiv = document.createElement("div");
-    checkboxDiv.classList.add("checkboxContainer");
+    let myDatePicker = document.createElement("input");
+    myDatePicker.setAttribute("type", "date");
+    myDatePicker.setAttribute("id", "myDateInput" + cardNumber);
 
-    //adding the completed checkbox
+    let myDatePickerLabel = document.createElement("label");
+    myDatePickerLabel.setAttribute("for", "datePicker");
+    myDatePicker.setAttribute("class", "datePicker");
+    myDatePickerLabel.innerHTML = "Due:";
+
+    myDateDiv.appendChild(myDatePickerLabel);
+    myDateDiv.appendChild(myDatePicker);
+    leftSideCardDiv.appendChild(myDateDiv);
+
+    if(datePicked != undefined)
+    {
+        myDatePicker.value = datePicked;
+    }
+
+
+    // ===== Button + Checkbox Section =====
+    let myButtonsContainer = document.createElement("div");
+    myButtonsContainer.classList.add("actionButtons");
+
+    let myEditButton = document.createElement("button");
+    myEditButton.classList.add("edit");
+    myEditButton.innerText = "✎";
+    myEditButton.setAttribute("id", cardNumber);
+
+    let myDeleteButton = document.createElement("button");
+    myDeleteButton.classList.add("delete");
+    myDeleteButton.innerText = "X";
+
     let completedCheckBox = document.createElement("input");
     completedCheckBox.setAttribute("type", "checkbox");
     completedCheckBox.classList.add("completedCheckbox");
@@ -124,9 +155,16 @@ function addCard(cardName, cardPriority, checkBoxValue, datePicked, fromStorage 
     completedCheckBoxLabel.setAttribute("for", "completedCheckbox");
     completedCheckBoxLabel.innerHTML = "Completed";
 
+    myButtonsContainer.appendChild(myEditButton);
+    myButtonsContainer.appendChild(myDeleteButton);
+    myButtonsContainer.appendChild(completedCheckBox);
+
+    newCard.appendChild(myButtonsContainer);
+
+
+    // ===== Event Listeners for Card Elements =====
     priorityMenuList.addEventListener("change", (e) =>
     {
-        //checks if the card already contains a class added by the select element. If so, remove it.
         if(newCard.classList.contains("lowPriority"))
         {
             newCard.classList.remove("lowPriority");
@@ -153,167 +191,100 @@ function addCard(cardName, cardPriority, checkBoxValue, datePicked, fromStorage 
             newCard.classList.add("highPriority");
         }
 
-        //logic to determine the checkbox state before it's added to the localStorage
         let isCheckboxChecked = false;
-
         const checkbox = newCard.querySelector(".completedCheckbox");
         if(checkbox.checked)
         {
             isCheckboxChecked = true;
         }
 
-        //update the card in localStorage when the card priority is changed, with the class name split on the priority class and the checkBox state
         let myValue = localStorage.getItem(currentCardNumber);
         let myValsSplit = myValue.split(",");
         myValsSplit[0] = newCard.classList.value.split(" ")[1];
         myValsSplit.toString();
         addCardToLocalStorage(currentCardNumber, myValsSplit);
         console.log(localStorage);
-
     });
 
-    newCard.appendChild(selectionDiv);
+    myDatePicker.addEventListener("change", () => 
+    {
+        if(myDatePicker !== "")
+        {
+            let value = localStorage.getItem(currentCardNumber);
+            let valsSplit = value.split(",");
+            valsSplit[3] = myDatePicker.value;
+            valsSplit.toString();
+            addCardToLocalStorage(currentCardNumber, valsSplit);
+            console.log(localStorage);
+        }
+    });
 
-    if (!fromStorage) {
-        addCardToLocalStorage(currentCardNumber, [myCardPriority, cardName, myCheckBoxValue, myDatePicked]);
-        cardNumber++;
-    }
-
-    let myButtonsContainer = document.createElement("div");
-    myButtonsContainer.classList.add("actionButtons");
-
-    let myEditButton = document.createElement("button");
-    myEditButton.classList.add("edit");
-    myEditButton.innerText = "Edit";
-    myEditButton.setAttribute("id", cardNumber);
-
-    //logic for editing a card's title
     myEditButton.addEventListener("click", (e) =>
     {        
-        //prompts the user to edit the card
         let newValue = prompt("What do you want to change the card name to?");
-
-        //changes the card's value to what the user entered.
         if(newValue != "")
         {
             myCardNameValue.innerHTML = newValue;
-            //add the card to the localStorage with the updated value
             let myValue = localStorage.getItem(currentCardNumber);
             let myValsSplit = myValue.split(",");
             myValsSplit[1] = newValue;
             myValsSplit.toString();
             addCardToLocalStorage(currentCardNumber, myValsSplit);
         }
-
     });
 
-    myButtonsContainer.appendChild(myEditButton);
-
-    let myDeleteButton = document.createElement("button");
-    myDeleteButton.classList.add("delete");
-    myDeleteButton.innerText = "Delete";
-    myButtonsContainer.appendChild(myDeleteButton);
-
-    //logic for deleting a card
     myDeleteButton.addEventListener("click", (e) =>
-        {
-            //set the seleted delete button to the clicked button
-            let selectedDeleteButton = e.target;
-            let parentDiv = selectedDeleteButton.parentNode.parentNode;
-            parentDiv.remove();
-            localStorage.removeItem(currentCardNumber);
-            console.log("card number when deleting: " + currentCardNumber);
-            console.log(localStorage);
-        });
-
-    newCard.appendChild(myButtonsContainer);
-
-    checkboxDiv.appendChild(completedCheckBoxLabel);
-    checkboxDiv.appendChild(completedCheckBox);
-
-    //add the date picker to the card
-    let myDateDiv = document.createElement("div");
-    myDateDiv.classList.add("dateDivContainer");
-
-    let myDatePicker = document.createElement("input");
-    myDatePicker.setAttribute("type", "date");
-    myDatePicker.setAttribute("id", "myDateInput" + cardNumber);
-
-    let myDatePickerLabel = document.createElement("label");
-    myDatePickerLabel.setAttribute("for", "datePicker");
-    myDatePicker.setAttribute("class", "datePicker");
-    myDatePickerLabel.innerHTML = "Due Date";
-
-    myDateDiv.appendChild(myDatePickerLabel);
-    myDateDiv.appendChild(myDatePicker);
-
-    if(datePicked != undefined)
     {
-        myDatePicker.value = datePicked;
+        let selectedDeleteButton = e.target;
+        let parentDiv = selectedDeleteButton.parentNode.parentNode;
+        parentDiv.remove();
+        localStorage.removeItem(currentCardNumber);
+        console.log("card number when deleting: " + currentCardNumber);
+        console.log(localStorage);
+    });
+
+    completedCheckBox.addEventListener("click", () => {
+        const checkbox = newCard.querySelector(".completedCheckbox");
+        let value = localStorage.getItem(currentCardNumber);
+        let valsSplit = value.split(",");
+        valsSplit[2] = checkbox.checked;
+        valsSplit.toString();
+        addCardToLocalStorage(currentCardNumber, valsSplit);
+        console.log(localStorage);
+    });
+
+
+    // ===== Local Storage Save =====
+    if (!fromStorage) {
+        addCardToLocalStorage(currentCardNumber, [myCardPriority, cardName, myCheckBoxValue, myDatePicked]);
+        cardNumber++;
     }
 
-    //when the date value is changed, output that to the console
-    myDatePicker.addEventListener("change", () => 
-        {
-            if(myDatePicker !== "")
-            {
-                let value = localStorage.getItem(currentCardNumber);
-                let valsSplit = value.split(",");
-                valsSplit[3] = myDatePicker.value;
-                valsSplit.toString();
-                addCardToLocalStorage(currentCardNumber, valsSplit);
-                console.log(localStorage);
-            }
-        });
-
-    //if the checkBoxValue is set to true, check the checkbox
     if(myCheckBoxValue != undefined)
     {
         myCheckBoxValue = (myCheckBoxValue === "true" || myCheckBoxValue === true);
         completedCheckBox.checked = myCheckBoxValue;
     }
 
-            
-    //update the checkbox variable in the local storage when the checkbox is clicked
-    completedCheckBox.addEventListener("click", () => {
-
-        const checkbox = newCard.querySelector(".completedCheckbox");
-
-            let value = localStorage.getItem(currentCardNumber);
-            let valsSplit = value.split(",");
-            valsSplit[2] = checkbox.checked;
-            valsSplit.toString();
-            addCardToLocalStorage(currentCardNumber, valsSplit);
-            console.log(localStorage);
-});
-
-
-    newCard.appendChild(myDateDiv);
-
-    newCard.appendChild(checkboxDiv);
-
-    //increment the cardNumber variable if the card is not being rebuilt from storage
     if (!fromStorage) {
         cardNumber++;
     }
+
     console.log(localStorage);
-
-    //update the myEditButtons nodeList after each new card is added
     myEditButtons = document.getElementsByClassName("edit");
-
-    
 }
 
+
+// ===== Local Storage Helper =====
 function addCardToLocalStorage(map, key)
 {
     localStorage.setItem(map, key);
 }
 
+
+// ===== Filter Buttons =====
 deleteCompletedButton.addEventListener("click", deleteCompleted);
 
-
-
-//hide all cards that are not a low priority
 lowPriorityButton.addEventListener("click", () =>
 {
     hideCards("mediumPriority");
@@ -321,7 +292,6 @@ lowPriorityButton.addEventListener("click", () =>
     displayCards("lowPriority");
 });
 
-//hide all cards that are not a medium priority
 mediumPriorityButton.addEventListener("click", () =>
 {
     hideCards("lowPriority");
@@ -329,7 +299,6 @@ mediumPriorityButton.addEventListener("click", () =>
     displayCards("mediumPriority");
 });
 
-//hide all cards that are not a high priority
 highPriorityButton.addEventListener("click", () =>
 {
     hideCards("lowPriority");
@@ -337,7 +306,6 @@ highPriorityButton.addEventListener("click", () =>
     displayCards("highPriority");
 });
 
-//hide all cards that are not a high priority
 displayAllCards.addEventListener("click", () =>
 {
     displayCards("lowPriority");
@@ -345,7 +313,8 @@ displayAllCards.addEventListener("click", () =>
     displayCards("highPriority");
 });
 
-//functions to only display cards with a certain class name
+
+// ===== Show/Hide Functions =====
 function hideCards(className)
 {
     const cards = document.getElementsByClassName(className);
@@ -379,20 +348,19 @@ function displayCards(className)
 }
 
 
+// ===== Delete Completed =====
 function deleteCompleted() {
     const cardList = document.getElementsByClassName("card");
 
     for (let i = cardList.length - 1; i >= 0; i--) 
+    {
+        const checkbox = cardList[i].querySelector(".completedCheckbox");
+        const card = cardList[i];
+    
+        if (checkbox && checkbox.checked) 
         {
-        // find the checkbox inside the current card
-            const checkbox = cardList[i].querySelector(".completedCheckbox");
-            const card = cardList[i];
-        
-            if (checkbox && checkbox.checked) 
-            {
-                card.remove();
-                localStorage.removeItem(card.id);
-            }
+            card.remove();
+            localStorage.removeItem(card.id);
         }
+    }
 }
-
